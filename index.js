109d9b521d78105665eb581e8ba009fdd2a12e85
Alden <63831735+AldenizenMC@users.noreply.github.com>
2021-02-17 12:30:59 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const axios = require("axios")
-const urls = ["https://alka-integrated-discordbot.glitch.me"]
+const urls = ["https://alka-integrated-discordbot.glitch.me", "https://bbh-bot.glitch.me"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
